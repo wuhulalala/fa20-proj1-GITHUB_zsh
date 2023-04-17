f604dffd864e7b87e2f2e9f5acd3a87b8b28c179
zsh <liscopye@gmail.com>
2023-04-17 17:20:19 +0800

    finish proj1

--- imageloader.c
+++ imageloader.c
@@ -25,17 +25,74 @@
 //Make sure that you close the file with fclose before returning.
 Image *readData(char *filename) 
 {
-	//YOUR CODE HERE
+	FILE *fp = fopen(filename, "r");
+	if (fp == NULL) {
+		printf("fail to open file %s\n", filename);
+		return NULL;
+	}
+	char buffer[3];
+	uint32_t rows, cols;
+	int rubbish;
+	fscanf(fp, "%s %u %u", buffer, &rows, &cols);
+	if (buffer[0] != 'P' || buffer[1] != '3') {
+		printf("file format error\n");
+		return NULL;
+	}
+	fscanf(fp, "%d", &rubbish);
+	if (rubbish != 255 || rows < 0 || cols < 0) {
+		printf("error\n");
+		return NULL;
+	}
+	// Malloc space for Image
+	Image * whole_image = (Image *) malloc(sizeof(Image));
+	whole_image -> rows = rows;
+	whole_image -> cols = cols;
+	int totpixel = rows * cols;
+	// Malloc space for Color
+	whole_image -> image = (Color **) malloc(totpixel * sizeof(Color *));
+	for (int count = 0; count < totpixel; count += 1) {
+		Color * temp = (Color *) malloc(sizeof(Color));
+		fscanf(fp, "%hhu %hhu %hhu", &temp -> R, &temp -> G, &temp -> B);
+		whole_image -> image[count] = temp;
+
+	}
+	fclose(fp);
+
+	return whole_image;
+
 }
 
 //Given an image, prints to stdout (e.g. with printf) a .ppm P3 file with the image's data.
 void writeData(Image *image)
 {
-	//YOUR CODE HERE
+	if (image == NULL) {
+		printf("NULL pointer\n");	
+	}
+	printf("P3\n%d %d\n255\n", image -> rows, image -> cols);
+	Color ** p = image -> image;
+	for (int i = 0; i < image -> rows; i += 1) {
+		for (int j = 0; j < image -> cols - 1; j += 1) {
+			printf("%3hhu %3hhu %3hhu   ", (*p) -> R, (*p) -> G, (*p) -> B);
+			p++;
+		}
+		printf("%3hhu %3hhu %3hhu\n", (*p) -> R, (*p) -> G, (*p) -> B);
+		p++;
+	}	
 }
 
 //Frees an image
 void freeImage(Image *image)
 {
-	//YOUR CODE HERE
+	if (image == NULL) {
+		printf("NULL pointer\n");
+	}
+	uint32_t totpixel = image -> rows * image -> cols;
+	for (int i = 0; i < totpixel; i += 1) {
+		free(image -> image[i]);
+		image -> image[i] = NULL;
+	}
+	free(image -> image);
+	image -> image = NULL;
+	free(image);
+	image = NULL;
 }
--- steganography.c
+++ steganography.c
@@ -17,17 +17,52 @@
 #include <stdlib.h>
 #include <inttypes.h>
 #include "imageloader.h"
-
+#define LSB(x) (x & 1)
 //Determines what color the cell at the given row/col should be. This should not affect Image, and should allocate space for a new Color.
 Color *evaluateOnePixel(Image *image, int row, int col)
 {
 	//YOUR CODE HERE
+	int position = row * image -> cols + col;
+	Color** p = image -> image;
+	p = p + position;
+	Color* cell = (Color*) malloc(sizeof(Color));
+	if (cell == NULL) {
+		printf("allocate fail\n");
+		return NULL;
+	}
+	cell -> R = (*p) -> R;
+	cell -> G = (*p) -> G;
+	cell -> B = (*p) -> B;
+	return cell;
 }
 
 //Given an image, creates a new image extracting the LSB of the B channel.
 Image *steganography(Image *image)
 {
 	//YOUR CODE HERE
+	if (image == NULL) {
+		printf("NULL pointer\n");
+		return NULL;
+	}
+	Image * new_image = (Image *) malloc(sizeof(Image));
+	int totpixels = image -> rows * image -> cols;
+	new_image -> image = (Color **) malloc(totpixels * sizeof(Color *));
+	new_image -> rows = image -> rows;
+	new_image -> cols = image -> cols;
+	Color** p = image -> image;
+	for (int i = 0; i < totpixels; i += 1) {
+		new_image -> image[i] = (Color *) malloc(sizeof(Color));
+		uint8_t temp = (*(p + i)) -> B;
+		temp = LSB(temp);
+		if (temp == 1) {
+			temp = 255;	
+		}
+		new_image -> image[i] -> R = temp;
+		new_image -> image[i] -> G = temp;
+		new_image -> image[i] -> B = temp;
+	}
+
+	return new_image;
 }
 
 /*
@@ -46,4 +81,17 @@ Make sure to free all memory before returning!
 int main(int argc, char **argv)
 {
 	//YOUR CODE HERE
+	char * filename = argv[1];
+	if (argc != 2) {
+		printf("usage: %s filename\n",argv[0]);
+		printf("filename is an ASCII PPM file (type P3) with maximum value 255.\n");
+		exit(-1);
+	}
+	Image * image1, *image2;
+	image1 = readData(filename);
+	image2 = steganography(image1);
+	writeData(image2);
+	freeImage(image1);
+	freeImage(image2);
+	return 0;
 }
